3d0bc8b225bc3b6dceae03151a70415ae1ef6fb9
daniel.velazquez <danicond97@gmail.com>
2022-02-01 18:15:13 -0600

    chore: added modules & dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "import/prefer-default-export": "off",
+    "no-restricted-exports": "off",
+  },
+};
